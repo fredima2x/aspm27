792601d9af6f2b4a71df45bd7065ee6d88287965
Frederick Martel <fsmpservers@gmail.com>
2026-08-01 17:26:23 +0200

    Fixed Name error

--- server/src/main.rs
+++ server/src/main.rs
@@ -16,10 +16,7 @@ mod models;
 async fn main() {
     let app = Router::new()
         .route("/", get(handler::hi))
-        .route(
-            "/chats",
-            get(handler::get_user_chats).post(handler::create_chat),
-        )
+        .route("/chats", get(handler::get_chats).post(handler::create_chat))
         .route("/chats/{id}", get(handler::get_chat))
         .route("/users", get(handler::get_users).post(handler::create_user))
         .route(
