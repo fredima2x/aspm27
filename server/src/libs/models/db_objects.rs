b4ba01bb9a661b58b7862c9958ecf7d132ba537f
Frederick Martel <fsmpservers@gmail.com>
2026-08-06 13:19:31 +0200

    Update db_objects.rs

--- server/src/libs/models/db_objects.rs
+++ server/src/libs/models/db_objects.rs
@@ -6,6 +6,10 @@ pub struct User {
     pub username: String,
     pub display_name: String,
     pub password_hash: String,
+    pub soft_delete: bool,
+    pub deleted_at: String,
+    pub created_at: String,
+    pub updated_at: String,
 }
 
 #[derive(Serialize, Deserialize, sqlx::FromRow)]
@@ -13,6 +17,10 @@ pub struct Chat {
     pub id: i64,
     pub chat_name: String,
     pub chat_desc: String,
+    pub soft_delete: bool,
+    pub deleted_at: String,
+    pub created_at: String,
+    pub updated_at: String,
 }
 
 #[derive(Serialize, sqlx::FromRow)]
@@ -21,5 +29,8 @@ pub struct Message {
     pub owner_id: i64,
     pub chat_id: i64,
     pub content: String,
+    pub soft_delete: bool,
+    pub deleted_at: String,
     pub created_at: String,
+    pub updated_at: String,
 }
