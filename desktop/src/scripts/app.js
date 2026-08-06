72bb355527c08ff143dc044cb4570325e404450a
Frederick Martel <fsmpservers@gmail.com>
2026-08-06 10:53:07 +0200

    Refactored APP.js and added some fetching functions

--- desktop/src/scripts/app.js
+++ desktop/src/scripts/app.js
@@ -1,7 +1,8 @@
 //// Configuration
 const baseURL = "http://127.0.0.1:3000";
 const messageInput = document.querySelector(".message-text-bar");
-const sendButton = document.querySelector(".send-button");
+const sendMessageButton = document.querySelector(".send-button");
+const deleteMessageButton = document.querySelector(".delete-button")
 const chatArea = document.querySelector(".chat-area");
 const storageKey = "messages";
 
@@ -23,6 +24,23 @@ function header() {
     "Authorization": `Bearer ${auth_token}`,
     "Content-Type": "application/json",
   };
+}
+
+async function get_chat_messages(chat_id, limit, offset) {
+  try {
+    const response = await fetch(`${baseURL}/chats/${chat_id}/messages`, {
+      method: "GET",
+      body: JSON.stringify({ limit, offset }),
+      headers: header(),
+    })
+    if (!response.ok) {
+      console.error(await response.text());
+      return null;
+    }
+    return await response.json();
+  } catch(error) {
+    console.error("Fetch error:", error);
+  }
 }
 
 async function register(username, password) {
@@ -150,10 +168,12 @@ async function updateChats() {
     container.appendChild(addContactButton);
 }
 
-function updateMessages() {
+async function updateMessages() {
   chatArea.innerHTML = "";
 
-  messages.forEach((entry) => {
+  if (!displayed_messages) {return}
+
+  displayed_messages.forEach((entry) => {
     const messageWrapper = document.createElement("div");
     messageWrapper.className = "message-me";
     messageWrapper.innerHTML = `
@@ -170,30 +190,23 @@ function updateMessages() {
   chatArea.scrollTop = chatArea.scrollHeight;
 }
 
-
-function displayMessage() {
-  if (!text) {
-    return;
-  }
-  messages.push({ message: text });
-  saveMessages();
-  renderMessages();
-  messageInput.value = "";
+// Handler
+async function handler_sendButton() {
+  // TODO API-Call
+  await update_ui();
+}
+async function handler_deleteButton() {
+  // TODO API-Call
+  await update_ui();
 }
 
-function deleteLastMessage() {
-  if (messages.length === 0) {
-    return;
-  }
-  messages.pop();
-  saveMessages();
-  renderMessages();
+//// System
+async function update_ui() {
+  await Promise.all([
+    updateChats(),
+    updateMessages(),
+  ]);
 }
-
-
-
-
-
 
 async function setup() {
 
@@ -205,16 +218,11 @@ async function setup() {
   }
   auth_token = loginResponse.token_string;
 
-  sendButton.addEventListener("click", displayMessage);
-  messageInput.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      displayMessage();
-    }
-  });
+  sendMessageButton.addEventListener("click", handler_sendButton);
+  deleteMessageButton.addEventListener("click", handler_deleteButton);
 
   // Setting Updates
-  setInterval(updateChats, 5000);
+  setInterval(() => { updateChats().catch(console.error); }, 5000);
 }
 
 setup();
