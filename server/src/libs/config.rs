80a7f2f5d8a131af87f5ac8362d93b9cda6eb363
fredima2x <fsmpservers@gmail.com>
2026-09-10 21:01:55 +0200

    Update config.rs

--- server/src/libs/config.rs
+++ server/src/libs/config.rs
@@ -1,3 +1,3 @@
 pub const SERVER_ADDRESS: &'static str = "127.0.0.1:3000";
 pub const SQLITE_DB_ADDRESS: &'static str = "sqlite:data.db";
-pub const TOKEN_SECRET: &'static str = "";
+pub const TOKEN_SECRET: &'static str = "ykhdlkrjdjdjdbdjxbfkltitiwtscacCbcnhlzlzt";
