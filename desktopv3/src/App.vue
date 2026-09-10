658d9f7e56110a36d9532be9ca7b4c1407b027b6
Frederick Martel <fsmpservers@gmail.com>
2026-09-10 17:58:40 +0200

    cc

--- desktopv3/src/App.vue
+++ desktopv3/src/App.vue
@@ -1,65 +1,11 @@
 <script setup>
-// Libraries
-import { getCurrentWindow } from '@tauri-apps/api/window';
-
-// Assets
-import fullscreenIcon from '/src/assets/fullscreen-icon.svg';
-import quitIcon from '/src/assets/quit-icon.svg';
-
-const currentWindow = getCurrentWindow();
-
-function toggleFullscreen() {
-    currentWindow.setFullscreen(!currentWindow.isFullscreen());
-}
-function quit() {
-    currentWindow.close();
-}
-function startDragging() {
-    currentWindow.startDragging();
-}
+import cHeader from './components/c-header.vue';
 </script>
 
 <template>
-    <header @mousedown="startDragging">
-        <div class="app-controls">
-            <button @click="toggleFullscreen" @mousedown.stop class="app-control-fullscreen"><img :src="fullscreenIcon" class="icon" alt="Fullscreen"></button>
-            <button @click="quit" @mousedown.stop class="app-control-quit"><img :src="quitIcon" class="icon" alt="Quit"></button>
-        </div>
-    </header>
-
+    <c-header />
     <router-view/>
-
 </template>
-
-<style scoped>
-header {
-    background-color: black;
-    display: flex;
-    align-items:  bottom;
-    padding: 5px;
-    margin: 0;
-    height: 20px;
-}
-
-.app-controls {
-    margin-left: auto;
-}
-
-header .icon {
-    width: 16px;
-    height: 16px;
-    filter: brightness(0) invert(1);
-}
-
-header button {
-    background: none;
-    border: none;
-    cursor: pointer;
-    padding: 4px;
-    margin: 0;
-}
-</style>
-
 
 <style>
 @import '/src/assets/styles/root.css';
