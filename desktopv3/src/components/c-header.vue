213e3b42455cd96d259992b7b3c640159da68bcd
Friedrich Brombach <friedrichbrombach@icloud.com>
2026-09-10 18:21:06 +0200

    fixed some formatting

--- desktopv3/src/components/c-header.vue
+++ desktopv3/src/components/c-header.vue
@@ -1,62 +1,70 @@
 <script setup>
-import { getCurrentWindow } from '@tauri-apps/api/window';
+import { getCurrentWindow } from "@tauri-apps/api/window";
 
-import fullscreenIcon from '../assets/fullscreen-icon.svg';
-import quitIcon from '../assets/quit-icon.svg';
+import fullscreenIcon from "../assets/fullscreen-icon.svg";
+import quitIcon from "../assets/quit-icon.svg";
 
 const currentWindow = getCurrentWindow();
 
 function toggleFullscreen() {
-    currentWindow.setFullscreen(!currentWindow.isFullscreen());
+  currentWindow.setFullscreen(!currentWindow.isFullscreen());
 }
 
 function quit() {
-    currentWindow.close();
+  currentWindow.close();
 }
 
 function startDragging() {
-    currentWindow.startDragging();
+  currentWindow.startDragging();
 }
 </script>
 
 <template>
-    <header @mousedown="startDragging">
-        <div class="app-controls">
-            <button @click="toggleFullscreen" @mousedown.stop class="app-control-fullscreen"><img :src="fullscreenIcon" class="icon" alt="Fullscreen"></button>
-            <button @click="quit" @mousedown.stop class="app-control-quit"><img :src="quitIcon" class="icon" alt="Quit"></button>
-        </div>
-    </header>
+  <header @mousedown="startDragging">
+    <div class="app-controls">
+      <button
+        @click="toggleFullscreen"
+        @mousedown.stop
+        class="app-control-fullscreen"
+      >
+        <img :src="fullscreenIcon" class="icon" alt="Fullscreen" />
+      </button>
+      <button @click="quit" @mousedown.stop class="app-control-quit">
+        <img :src="quitIcon" class="icon" alt="Quit" />
+      </button>
+    </div>
+  </header>
 </template>
 
 <style scoped>
 header {
-    background-color: var(--theme-gray);
-    display: fixed;
-    padding: 5px;
-    margin: 0;
-    right: 0;
-    left: 0;
-    top: 0;
-    height: 20px;
-    border-bottom: solid 2px var(--theme-primary);
+  background-color: var(--theme-gray);
+  display: fixed;
+  padding: 5px;
+  margin: 0;
+  right: 0;
+  left: 0;
+  top: 0;
+  height: 20px;
+  border-bottom: solid 2px var(--theme-primary);
 }
 
 .app-controls {
-   display: flex;
-   align-items: center;
-   justify-content: flex-start;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
 }
 
 header .icon {
-    width: 16px;
-    filter: brightness(0) invert(1);
+  width: 16px;
+  filter: brightness(0) invert(1);
 }
 
 header button {
-    background: none;
-    border: none;
-    cursor: pointer;
-    padding: 4px;
-    margin: 0;
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 4px;
+  margin: 0;
 }
 </style>
