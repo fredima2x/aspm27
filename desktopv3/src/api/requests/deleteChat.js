e12de92cc95a92728c5f4095b8602af01fdb990d
Friedrich Brombach <friedrichbrombach@icloud.com>
2026-09-14 15:55:20 +0200

    cc

--- desktopv3/src/api/requests/deleteChat.js
+++ desktopv3/src/api/requests/deleteChat.js
@@ -1,6 +1,6 @@
 import { apiFetch } from "../client.js";
 
-export function deleteChat(chat) {
+export function delete_chat(chat) {
   const chatId = chat.chatId;
   return apiFetch(`/users/${chatId}`, {
     method: 'DELETE'
